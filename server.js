2e36f946c6441f94a42014a5ee3756559a4387c2
Jackson Donovan <iinventedtwitter@gmail.com>
2017-07-09 16:45:22 -0600

    time to deploy

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 
 app.get('/',(req,res)=>{
-  res.render('index',{msg:"'Welcome to Kath's baskets!"})
+  res.render('index',{msg:"Welcome to Kathy's baskets!"})
 })
 
 app.listen(port,(req,res)=>{
--- server/components/app.module.js
+++ server/components/app.module.js
@@ -1,27 +1,45 @@
-angular.module('app',["ngRoute"])
-  .config(["$routeProvider",function($routeProvider){
+var app = angular.module('app',["ngRoute"])
 
-    $routeProvider
-     .when('/gallery',{
-      //  component: 'gallery',
-       templateUrl: '/templates/gallery.html',
-       controller: 'buybaskets'
-     })
-     .when('/about',{
-       templateUrl: '/templates/about.html'
-     })
-     .when('/contact',{
-       templateUrl: '/templates/contact.html'
-     }).otherwise({
-       redirectTo: '/gallery'
-     })
+app.controller('buybaskets',function($scope){
+  $scope.title = 'Click any basket to add it to your cart'
+  $scope.yourcart = 0
+  $scope.atc_one = function(){
+    $scope.yourcart += 16.99
+  }
+  $scope.atc_two = function(){
+    $scope.yourcart += 22.99
+  }
+  $scope.atc_three = function(){
+    $scope.yourcart += 12.99
+  }
+  $scope.atc_four = function(){
+    $scope.yourcart += 10.99
+  }
+})
 
-  }])
-  .controller('buybaskets',function(){
-    const vm = this
-    vm.yourcart = 0
-  })
-  .component('gallery',{
+app.config(["$routeProvider",function($routeProvider){
+  $routeProvider
+   .when('/gallery',{
+
+     templateUrl: '/templates/gallery.hbs',
+     controller: 'buybaskets'
+   })
+   .when('/about',{
+     templateUrl: '/templates/about.hbs',
+     controller: 'londonCtrl'
+   })
+   .when('/contact',{
+     templateUrl: '/templates/contact.hbs'
+   })
+   .when('/postpurchase',{
+     templateUrl: '/templates/postpurchase.hbs'
+   })
+   .otherwise({
+     redirectTo: '/gallery'
+   })
+}])
+
+app.component('gallery',{
     controller: function(){
       const vm = this
       vm.title = 'Hello'
